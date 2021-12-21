4ab78b683cce154793adb110ef0fd987f8ada51c
svejda <svejda@g8mb.cz>
2021-12-21 15:44:10 +0100

    obrazky

--- script.js
+++ script.js
@@ -77,6 +77,7 @@ async function createItem() {
   body.obj.roknar = document.getElementById("roknarozeni").value;
   body.obj.email = document.getElementById("email").value;
   body.obj.potvrzeno = document.getElementById("potvrzeno").checked;
+  body.obj.obrazek = picUrl;
   let response = await fetch(url, {"method":"POST", "body": JSON.stringify(body)});
   let data = await response.json();
 
@@ -104,6 +105,11 @@ async function editItem(id) {
   document.getElementById("roknarozeni").value = o.roknar;
   document.getElementById("email").value = o.email;
   document.getElementById("potvrzeno").checked = o.potvrzeno;
+  if (o.obrazek) {
+    document.getElementById("picture").src = o.obrazek;
+  } else {
+    document.getElementById("picture").src = "avatar.png";
+  }
 
 }
 
@@ -134,9 +140,13 @@ async function updateItems() {
   console.log(data);
 
   let s = "<table class='table'>";
-  s = s + "<tr><th>Jméno</th><th>Příjmení</th><th>Rok narození</th><th>E-mail</th><th>Potvrzeno</th><th></th></tr>";
+  s = s + "<tr><th> </th><th>Jméno</th><th>Příjmení</th><th>Rok narození</th><th>E-mail</th><th>Potvrzeno</th><th></th></tr>";
   for (let m of data.items) {
-    s = s + "<tr><td>" + m.obj.jmeno + "</td><td>" + m.obj.prijmeni + "</td><td>" + m.obj.roknar + "</td><td>" + m.obj.email + "</td><td>" + m.obj.potvrzeno + "</td>";
+    s = s + "<tr><td>";
+    if (m.obj.obrazek) {
+      s = s + "<img src='" + m.obj.obrazek + "' height='32'>";
+    }
+    s = s + "</td><td>" + m.obj.jmeno + "</td><td>" + m.obj.prijmeni + "</td><td>" + m.obj.roknar + "</td><td>" + m.obj.email + "</td><td>" + m.obj.potvrzeno + "</td>";
     s = s + "<td>";
     s = s + "<button onclick='editItem(" + m.id +")'>upravit</button>";
     s = s + "<button onclick='deleteItem(" + m.id +")'>odstranit</button>";
@@ -180,3 +190,48 @@ function onLoad() {
 
   //document.getElementById("message").addEventListener("keydown", onKeyDown);
 }
+
+
+function getBase64Image(img, resize = false) {
+  let cnv = document.createElement("canvas");
+  if (resize) {
+      cnv.width = img.width;
+      cnv.height = img.height;
+  } else {
+      cnv.width = img.naturalWidth;
+      cnv.height = img.naturalHeight;
+  }
+
+  let ctx = cnv.getContext("2d");
+  ctx.drawImage(img, 0, 0, cnv.width, cnv.height);
+
+  return cnv.toDataURL();
+}
+
+let picUrl;
+function savePicture() {
+  const file = document.getElementById('file_pic').files[0];
+  if (!file) return; //stisknuto Storno
+  let tmppath = URL.createObjectURL(file); //create temporary file
+  let img = document.getElementById("picture");
+  img.src = tmppath;
+  img.onload = function(){
+      let url = 'https://nodejs-3260.rostiapp.cz/crud/upload';
+      let body = {};
+      body.appId = "f37620849972633644bbc5e1817f8227";
+      body.fileName = file.name;
+      body.contentType = file.type;
+      body.data = getBase64Image(img, true); //convert to Base64
+      fetch(url, {method: "POST", body: JSON.stringify(body)})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            picUrl = 'https://nodejs-3260.rostiapp.cz/' + data.savedToFile;
+            img.onload = null;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+  };
+}
+
